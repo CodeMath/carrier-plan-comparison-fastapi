2ca324bde06c6f07506bd8b644c788d1aa641760
CodeMath <xyrho123@naver.com>
2023-06-02 14:54:45 +0900

    change api call

--- api/app/router/user_plan.py
+++ api/app/router/user_plan.py
@@ -1,9 +1,10 @@
-from fastapi import Depends, APIRouter, Header, Query, HTTPException
+from fastapi import Depends, APIRouter, Query, HTTPException
 from typing_extensions import Annotated
+from typing import Union
 from pydantic import Required
 
-from ..internal.planKT import PlanKT, fake_plan, CombinedDiscount
-from ..dependency import CarrierPlanQueryParams
+from ..internal.planKT import PlanKT, fake_plan, InternatCombination, fake_internet
+from ..dependency import carrier_type
 
 
 router = APIRouter(
@@ -18,28 +19,31 @@ router = APIRouter(
     summary="Get list KT carrier plan",
     response_description="Get list KT carrier plan",
 )
-async def get_items(commons: CarrierPlanQueryParams = Depends(CarrierPlanQueryParams)):
+async def get_items(
+    price: Annotated[int, Query(ge=0)] = 0,
+    tp: Annotated[str, Query(examples=carrier_type)] = "all",
+):
     """
     Get list of KT carrier plan for "가족 프리미엄 결합", "총액결합" and "싱글결합"
     - price: price
     - tp: 5g / LTE / all
     """
-    if commons.price:
+    if price:
         ge_plan = []
-        for plan in fake_plan:
-            if plan["price"] >= commons.price:
-                if commons.tp == "all":
+        for plan in fake_plan.values():
+            if plan["price"] >= price:
+                if tp == "all":
                     ge_plan.append(plan)
-                elif commons.tp.upper() == "5G" and plan["carrier"] == "5G":
+                elif tp.upper() == "5G" and plan["carrier"] == "5G":
                     ge_plan.append(plan)
-                elif commons.tp.upper() == "LTE" and plan["carrier"] == "LTE":
+                elif tp.upper() == "LTE" and plan["carrier"] == "LTE":
                     ge_plan.append(plan)
         if ge_plan:
             return ge_plan
         else:
             raise HTTPException(status_code=404)
 
-    return fake_plan
+    return [plan for plan in fake_plan.values()]
 
 
 def is_base_line(plan: list[PlanKT]):
@@ -51,14 +55,31 @@ def is_base_line(plan: list[PlanKT]):
     summary="combination of KT carrier plan",
     response_description="combination of KT carrier plan",
 )
-async def select_carrier(q: Annotated[list[str], Query(min_lenght=1)] = Required):
+async def select_carrier(
+    q: Annotated[
+        Union[list[str], None],
+        Query(
+            min_lenght=1,
+            examples=fake_plan,
+        ),
+    ],
+    ith: Annotated[str, Query(examples=fake_internet)],
+):
     """
     - **q**: each KT plan name
     """
-    stored_plan = [plan for plan in fake_plan for name in q if plan["name"] == name]
-    # stored_plan_model = list[PlanKT[stored_plan]]
+    try:
+        stored_plan = [fake_plan[name] for name in q]
+        base_line = is_base_line(stored_plan)
+    except KeyError:
+        raise HTTPException(status_code=404)
 
-    base_line = is_base_line(stored_plan)
+    try:
+        internet = fake_internet[ith]
+        base_internet = InternatCombination(**internet)
+    except KeyError:
+        internet = {}
+        base_internet = InternatCombination(internet=ith)
 
     # single combination
     if len(q) == 1:
@@ -68,16 +89,14 @@ async def select_carrier(q: Annotated[list[str], Query(min_lenght=1)] = Required
                 "contract_discount_25%": base_line["price"] * 0.25,
                 "single_contract_discount_25%": base_line["price"] * 0.25,
                 "discount_sum": base_line["price"] * 0.5,
-                "internet": {
-                    "plan": "인터넷 베이직 3년 약정",
-                    "mbps": 500,
-                    "price": 31350,
-                },
-                "payment": base_line["price"] * 0.5 + 31350,
+                "internet": base_internet,
+                "payment": base_line["price"] * 0.5 + base_internet.price,
             }
         )
     else:
-        pass
+        # check 2 case
+        for plan in stored_plan:
+            pass
 
     # sum_price = 0
     # for splan in stored_plan:
